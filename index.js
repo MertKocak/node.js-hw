ed6625222f51f2d77f39b47ec2c9142cf883da70
Mert Koçak <74009802+MertKocak@users.noreply.github.com>
2024-10-06 22:29:32 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,26 +1,27 @@
-const http = require('http');
-const port = 5000;
+const fs = require('node:fs');
 
-const server = http.createServer((req, res) => {
-    const url = req.url;
-  
-    if (url === "/") {
-      res.writeHead(200, { "Content-Type": "text/html" });
-      res.write("<h2>INDEX SAYFASI</h2>");
-    } else if (url === "/hakkimda") {
-      res.writeHead(200, { "Content-Type": "text/html" });
-      res.write("<h2>HAKKIMDA SAYFASI</h2>");
-    } else if (url === "/iletisim") {
-      res.writeHead(200, { "Content-Type": "text/html" });
-      res.write("<h2>İLETİŞİM SAYFASI</h2>");
+fs.writeFile('employees.json', `{"name": "Employee 1 Name", "salary": 2000}`, err => {
+    if (err) {
+        console.error(err);
     } else {
-      res.writeHead(404, { "Content-Type": "text/html" });
-      res.write("<h2>404 SAYFA BULUNAMADI</h2>");
+        console.log("dosya oluşturuldu.");
     }
-  
-    res.end();
-  });
+});
 
-  server.listen(port, () => {
-    console.log(`Sunucu port ${port} de başlatıldı.`);
+fs.readFile('employees.json', 'utf8', (err, data) => {
+    if (err) {
+        console.error(err);
+        return;
+    }
+    console.log(data);
+});
+
+fs.appendFile('employees.json', '\n,{"name": "Employee 2 Name", "salary": 4000}', function (err) {
+    if (err) throw err;
+    console.log('dosya güncellendi');
+});
+
+fs.unlink('employees.json', function (err) {
+    if (err) throw err;
+    console.log('dosya silindi');
   });
